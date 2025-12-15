fd0dc2f068722ae5fe042c7b46c5f2e0d27902da
nshankar28-eng <nshankar28@bentleyschool.org>
2025-12-15 08:43:55 -0800

    Add files via upload

--- CSP_4_03_Try_Except.py
+++ CSP_4_03_Try_Except.py
@@ -1,24 +1,33 @@
-#No using the built in type check function
-#https://www.w3schools.com/python/python_try_except.asp
-
-
-def sum(arr : list) -> int:
+def sum(arr: list) -> int:
     """
-    Modify the function such that it returns the sum of all numebrs within the given list.
+    Modify the function such that it returns the sum of all numbers within the given list.
     :param arr:
     :return:
     """
-    pass
+    total = 0
+    for num in arr:
+        total += num
+    return total
 
-def cleanData(rawData : list) ->list:
+
+def cleanData(rawData: list) -> list:
     """
     modify the function such that it takes in a list as an argument will return a new list that
-     contains only the valeus that can be typecast to a float.
+    contains only the values that can be typecast to a float.
     :param rawData:
     :return:
     """
-    pass
-def unreliableCalculator(divisors : list) -> list:
+    result = []
+    for item in rawData:
+        try:
+            float(item)
+            result.append(item)
+        except:
+            pass
+    return result
+
+
+def unreliableCalculator(divisors: list) -> list:
     """
     Modify the function such that it takes in a list as an argument and returns a new list where each
     index is 100 divided by the values from the input list.
@@ -27,32 +36,49 @@ def unreliableCalculator(divisors : list) -> list:
     :param divisors:
     :return:
     """
-    pass
+    result = []
+    for divisor in divisors:
+        try:
+            value = 100 / divisor
+            result.append(value)
+        except Exception as e:
+            result.append(type(e).__name__)
+    return result
 
 
-def upperAll(arr : list) -> None:
+def upperAll(arr: list) -> None:
     """
-    Modiy the function such that is uppercases all strings within the given argument list.
-    The string method .upper() turns all characters in as tirng uppercase.
-    You should mpdify the original list not return a new list.
+    Modify the function such that it uppercases all strings within the given argument list.
+    The string method .upper() turns all characters in a string uppercase.
+    You should modify the original list not return a new list.
     :param arr:
     :return:
     """
-    x = "hello"
-    print(x)
-    x = x.upper()
-    print(x)
+    for i in range(len(arr)):
+        try:
+            arr[i] = arr[i].upper()
+        except:
+            pass
 
 
-def firstItems(arr : list) -> list:
+def firstItems(arr: list) -> list:
     """
     Modify the function below such that given a list of values. Many of the list elements will be lists
     themselves. For any list element that is a list grab the first element from that list. If the list
     element is not a list then just grab the value itself.
     Create a new list of all the first indexes of inner lists or just values themselves.
-    Example firstItems( [[1,2],[3,4],[5,6],[7,8]],9 ) == [1,3,5,7,9]
+    Example firstItems([[1,2],[3,4],[5,6],[7,8],9]) == [1,3,5,7,9]
     :param arr:
     :return:
     """
-    pass
+    result = []
+    for item in arr:
+        try:
+            # Try to access the first element (assumes it's a list)
+            result.append(item[0])
+        except:
+            # If it fails, it's not a list, so just append the item itself
+            result.append(item)
+    return result
+
 
